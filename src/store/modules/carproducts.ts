d34da2c76c689daa4636be135cdd25cc78919263
Erick2418 <erickjoellimon@outlook.com>
2021-11-03 11:09:14 -0500

    api otra vez

--- src/store/modules/carproducts.ts
+++ src/store/modules/carproducts.ts
@@ -62,7 +62,7 @@ const actions: ActionTree<ProductsState,any>={
         
         try {
 
-            apiProducts = await axios.get(`http://localhost:3002/api/producto/${idProducto}`).then(({data}) => {
+            apiProducts = await axios.get(`https://backend-app-store.herokuapp.com/api/producto/${idProducto}`).then(({data}) => {
             //  https://backend-app-store.herokuapp.com/api/producto/
             // http://localhost:3002/api/producto/
               commit('addCarproduct',data.producto);
